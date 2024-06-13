5a650f9c0f39f80a1a43fee3f4683344001d6aab
Shantanu <shantanu.ingale22@gmail.com>
2024-06-13 14:35:45 +0530

    final changes

--- src/functions/getCoinData.js
+++ src/functions/getCoinData.js
@@ -18,7 +18,7 @@ export const getCoinData = (id) => {
     },
   })
     .then((response) => {
-      console.log(response.data);
+      // console.log(response.data);
       return response.data;
     })
 
